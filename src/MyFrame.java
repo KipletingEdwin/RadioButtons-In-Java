6cc229070e209f07d4722845f603e067b8336a94
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-12 20:27:03 +0300

    add a JFrame to MyFrame class

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,2 +1,23 @@
-public class MyFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.pack();
+        this.setVisible(true);
+    }
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
