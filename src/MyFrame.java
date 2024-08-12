6e9cdcfe84ba5945a4da72bfa60a2940cc262af3
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-12 20:42:07 +0300

    add three instances of JRadioButton

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -3,16 +3,20 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class MyFrame extends JFrame implements ActionListener {
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+        JRadioButton pizzaButton = new JRadioButton();
+        JRadioButton hamburgerButton = new JRadioButton();
+        JRadioButton hotdogButton = new JRadioButton();
+
         this.pack();
         this.setVisible(true);
     }
-
-
 
 
 
